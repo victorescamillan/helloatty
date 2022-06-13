b5ea7a9dfa25875d485f017a32bd3059ad05cbc8
erylljoy24 <eryll.salamanes24@gmail.com>
2022-06-13 13:46:42 +0800

    Implemented forgot password UI and reset password UI

--- lib/ui/screens/forgot_password/forgot_password_screen.dart
+++ lib/ui/screens/forgot_password/forgot_password_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:helloatty/ui/screens/forgot_password/widgets/email_forgot_credentials.dart';
 import 'package:helloatty/ui/widgets/app_scaffold.dart';
+import 'package:helloatty/ui/widgets/auth_side_image.dart';
 import 'package:helloatty/ui/widgets/temp_container.dart';
+import 'package:helloatty/utils/response.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({Key? key}) : super(key: key);
@@ -12,10 +15,36 @@ class ForgotPasswordScreen extends StatefulWidget {
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   @override
   Widget build(BuildContext context) {
-    return const AppScaffold(
-      body: TempContainer(
-        title: 'ForgotPasswordScreen',
-      ),
+    return AppScaffold(
+      body: _displayForgotPassword()
     );
+  }
+
+  _displayForgotPassword() {
+    if (Responsive.isDesktop(context)) {
+      return Row(
+        children: const [
+          Expanded(flex: 2, child: EmailForgotCredentials()),
+          Expanded(flex: 4, child: AuthSideImage()),
+        ],
+      );
+    } else if (Responsive.isTablet(context)) {
+      return Row(
+        children: const [
+          Expanded(flex: 1, child: EmailForgotCredentials()),
+          Expanded(flex: 1, child: AuthSideImage()),
+        ],
+      );
+    } else {
+      return Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: const [
+              EmailForgotCredentials(),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
