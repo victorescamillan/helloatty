beffe4f201ad987be6a1bf9534189490536ef95a
erylljoy24 <eryll.salamanes24@gmail.com>
2022-06-29 01:03:04 +0800

    revise card on bookings

--- lib/ui/screens/booking/widgets/booking_grid_view.dart
+++ lib/ui/screens/booking/widgets/booking_grid_view.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:helloatty/constants/app_colors.dart';
 import 'package:helloatty/constants/app_dimens.dart';
 import 'package:helloatty/constants/app_styles.dart';
+import 'package:helloatty/ui/widgets/app_buttons/default_button.dart';
+import 'package:helloatty/ui/widgets/app_buttons/default_outlined_button.dart';
 import 'package:helloatty/ui/widgets/app_cards/default_card.dart';
+import 'package:helloatty/ui/widgets/app_inputs/bordered_textfield.dart';
 
 class BookingGridView extends StatefulWidget {
   const BookingGridView({Key? key}) : super(key: key);
@@ -25,203 +28,213 @@ class _BookingGridViewState extends State<BookingGridView> {
           children: [
             DefaultCard(
               width: 300,
-              height: 420,
+              height: 470,
               content: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       const CircleAvatar(
                         radius: size40,
                         backgroundImage: NetworkImage(
                             'https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/05/thor-recap-feature.jpg'),
                       ),
-                      const SizedBox(
-                        width: 85,
-                      ),
+                      const SizedBox(width: 85,),
                       Column(
                         children: const [
-                          Text(
-                            '10',
-                            style: kHeaderTextStyle,
-                          ),
-                          Text(
-                            'March',
-                            style: kActiveColorTextStyle,
-                          ),
+                          Text('10',
+                            style: kHeaderTextStyle,),
+                          Text('March',
+                            style: kActiveColorTextStyle,),
                         ],
                       )
                     ],
                   ),
-                  const SizedBox(
-                    height: size15,
+                  const SizedBox(height: size10,),
+                  const Text('Joshua Barnes', style: kHeader1TextStyle,),
+                  Text('270 Robles Manggahan, Quezon City', style: kTitleTextStyle.copyWith(color: appColor, fontWeight: FontWeight.bold),),
+                  const SizedBox(height: size20,),
+                  const Text('Schedule', style: kTitleTextStyle,),
+                  const SizedBox(height: size5,),
+                  Text('Wed, March 10, 2021 | 10:30 am', style: kTitleTextStyle.copyWith(color: kRed),),
+
+                  const SizedBox(height: size20,),
+                  Text('Message:', style: kTitleTextStyle.copyWith(fontSize: 12),),
+                  Text("I'm looking for a lawyer that can help me about finance", style: kTitleTextStyle.copyWith(fontSize: 12),),
+
+                  const SizedBox(height: size20,),
+                  const Text('Enter Zoom Link', style: kTitleTextStyle,),
+                  Container(
+                    height: size30,
+                    padding: const EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                        color: disabledColor,
+                        border: Border.all(
+                          color: disabledColor,
+                        ),
+                        borderRadius: const BorderRadius.all(Radius.circular(5))
+                    ),
+                    child: TextField(
+                      cursorColor: kGrey,
+                      style: kTitleTextStyle.copyWith(fontSize: 13),
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Input link here',
+                      ),
+                    )
                   ),
-                  const Text(
-                    'Joshua Barnes',
-                    style: kHeader1TextStyle,
-                  ),
-                  const Text(
-                    'Financial Lawyer',
-                    style: kSubtitle2TextStyle,
-                  ),
-                  const Text(
-                    '15 years of Experience | Class of 2000',
-                    style: kSubtitle2TextStyle,
-                  ),
-                  const SizedBox(
-                    height: size20,
-                  ),
+                  const SizedBox(height: size20,),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Icon(Icons.school),
-                      const SizedBox(
-                        width: size5,
-                      ),
-                      Text(
-                        'University of the Philippines',
-                        style: kTitleTextStyle.copyWith(color: appColor),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: size20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: const [
-                      Icon(Icons.person),
-                      SizedBox(
-                        width: size5,
-                      ),
-                      Expanded(
-                        child: Text(
-                          'Specialized in Finance and Security',
-                          style: kBlueTextStyle,
+                      Container(
+                        height: size30,
+                        width: size120,
+                        padding: const EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: appColor,
+                            ),
+                            borderRadius: const BorderRadius.all(Radius.circular(5))
+                        ),
+                        child: GestureDetector(
+                          child: Text(
+                            'Cancel',
+                            textAlign: TextAlign.center,
+                            style: kTitleTextStyle.copyWith(color: appColor),
+                          ),
                         ),
                       ),
+                      Container(
+                        height: size30,
+                        width: size120,
+                        padding: const EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                            color: appColor,
+                            border: Border.all(
+                              color: appColor,
+                            ),
+                            borderRadius: const BorderRadius.all(Radius.circular(5))
+                        ),
+                        child: GestureDetector(
+                          child: Text(
+                            'Confirm',
+                            textAlign: TextAlign.center,
+                            style: kTitleTextStyle.copyWith(color: kWhite),
+                          ),
+                        ),
+                      )
                     ],
-                  ),
-                  const SizedBox(
-                    height: size20,
-                  ),
-                  const Text(
-                    'Schedule',
-                    style: kTitleTextStyle,
-                  ),
-                  const Text(
-                    'Wed, March 10, 2021 | 10:30 am',
-                    style: kTitleTextStyle,
-                  ),
-                  const Text(
-                    'https://zoom.us/meetings',
-                    style: kTitleTextStyle,
                   )
                 ],
               ),
             ),
-            const SizedBox(
-              width: size30,
-            ),
+            const SizedBox(width: size30,),
             DefaultCard(
               width: 300,
-              height: 420,
+              height: 470,
               content: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       const CircleAvatar(
                         radius: size40,
                         backgroundImage: NetworkImage(
                             'https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/05/thor-recap-feature.jpg'),
                       ),
-                      const SizedBox(
-                        width: 85,
-                      ),
+                      const SizedBox(width: 85,),
                       Column(
                         children: const [
-                          Text(
-                            '10',
-                            style: kHeaderTextStyle,
-                          ),
-                          Text(
-                            'March',
-                            style: kActiveColorTextStyle,
-                          ),
+                          Text('10',
+                            style: kHeaderTextStyle,),
+                          Text('March',
+                            style: kActiveColorTextStyle,),
                         ],
                       )
                     ],
                   ),
-                  const SizedBox(
-                    height: size15,
+                  const SizedBox(height: size10,),
+                  const Text('Joshua Barnes', style: kHeader1TextStyle,),
+                  Text('270 Robles Manggahan, Quezon City', style: kTitleTextStyle.copyWith(color: appColor, fontWeight: FontWeight.bold),),
+                  const SizedBox(height: size20,),
+                  const Text('Schedule', style: kTitleTextStyle,),
+                  const SizedBox(height: size5,),
+                  Text('Wed, March 10, 2021 | 10:30 am', style: kTitleTextStyle.copyWith(color: kRed),),
+
+                  const SizedBox(height: size20,),
+                  Text('Message:', style: kTitleTextStyle.copyWith(fontSize: 12),),
+                  Text("I'm looking for a lawyer that can help me about finance", style: kTitleTextStyle.copyWith(fontSize: 12),),
+
+                  const SizedBox(height: size20,),
+                  const Text('Enter Zoom Link', style: kTitleTextStyle,),
+                  Container(
+                      height: size30,
+                      padding: const EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                          color: disabledColor,
+                          border: Border.all(
+                            color: disabledColor,
+                          ),
+                          borderRadius: const BorderRadius.all(Radius.circular(5))
+                      ),
+                      child: TextField(
+                        cursorColor: kGrey,
+                        style: kTitleTextStyle.copyWith(fontSize: 13),
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Input link here',
+                        ),
+                      )
                   ),
-                  const Text(
-                    'Joshua Barnes',
-                    style: kHeader1TextStyle,
-                  ),
-                  const Text(
-                    'Financial Lawyer',
-                    style: kSubtitle2TextStyle,
-                  ),
-                  const Text(
-                    '15 years of Experience | Class of 2000',
-                    style: kSubtitle2TextStyle,
-                  ),
-                  const SizedBox(
-                    height: size20,
-                  ),
+                  const SizedBox(height: size20,),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: const [
-                      Icon(Icons.school),
-                      SizedBox(
-                        width: size5,
-                      ),
-                      Text(
-                        'University of the Philippines',
-                        style: kBlueTextStyle,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: size20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: const [
-                      Icon(Icons.person),
-                      SizedBox(
-                        width: size5,
-                      ),
-                      Expanded(
-                        child: Text(
-                          'Specialized in Finance and Security',
-                          style: kBlueTextStyle,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: size30,
+                        width: size120,
+                        padding: const EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: appColor,
+                            ),
+                            borderRadius: const BorderRadius.all(Radius.circular(5))
+                        ),
+                        child: GestureDetector(
+                          child: Text(
+                            'Cancel',
+                            textAlign: TextAlign.center,
+                            style: kTitleTextStyle.copyWith(color: appColor),
+                          ),
                         ),
                       ),
+                      Container(
+                        height: size30,
+                        width: size120,
+                        padding: const EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                            color: appColor,
+                            border: Border.all(
+                              color: appColor,
+                            ),
+                            borderRadius: const BorderRadius.all(Radius.circular(5))
+                        ),
+                        child: GestureDetector(
+                          child: Text(
+                            'Confirm',
+                            textAlign: TextAlign.center,
+                            style: kTitleTextStyle.copyWith(color: kWhite),
+                          ),
+                        ),
+                      )
                     ],
-                  ),
-                  const SizedBox(
-                    height: size20,
-                  ),
-                  const Text(
-                    'Schedule',
-                    style: kTitleTextStyle,
-                  ),
-                  const Text(
-                    'Wed, March 10, 2021 | 10:30 am',
-                    style: kTitleTextStyle,
-                  ),
-                  const Text(
-                    'https://zoom.us/meetings',
-                    style: kTitleTextStyle,
                   )
                 ],
               ),
-            )
+            ),
           ],
         )
       ],
